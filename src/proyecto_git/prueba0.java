ef461eca90e27086ad2941cd07340e3093beea0b
EffenitRuft <60186333+EffenitRuft@users.noreply.github.com>
2020-01-29 16:49:10 +0100

    Nuevo

---
+++ src/proyecto_git/prueba0.java
@@ -0,0 +1,7 @@
+public class{
+public static void main(String[] args) {
+		System.out.println("Hola mundo");
+		System.out.println("Tengo un compañero bastante meh.");
+		System.out.println("Y encima lo hago mal");
+	}
+}
