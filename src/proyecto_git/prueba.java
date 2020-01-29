75d4eb3b8bd326b62f2872afb9aa24e7881003c5
Effenit <Effenit@DESKTOP-6T7I4UP>
2020-01-29 16:02:49 +0100

    Nuevos cambios

--- src/proyecto_git/prueba.java
+++ src/proyecto_git/prueba.java
@@ -4,6 +4,6 @@ public class prueba {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 		System.out.println("Tengo un compañero bastante meh.");
-		
+		System.out.println("Y encima lo hago mal");
 	}
 }
