2a591af1826412a9595504f2035a6937b469c337
Effenit <Effenit@DESKTOP-6T7I4UP>
2020-01-29 16:01:12 +0100

    Cambios

--- src/proyecto_git/prueba.java
+++ src/proyecto_git/prueba.java
@@ -1,2 +1,9 @@
+package proyecto_git;
 
-System.out.println("Hola mundo");
+public class prueba {
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+		System.out.println("Tengo un compañero bastante meh.");
+		
+	}
+}
